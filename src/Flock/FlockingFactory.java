319d7bf2ea9cc0de52ea04054ab9a988cec4db0f
Daniel Nunez <143371163+nunez-daniel@users.noreply.github.com>
2024-04-16 16:05:42 -0700

    Add files via upload

--- src/Flock/FlockingFactory.java
+++ src/Flock/FlockingFactory.java
@@ -2,12 +2,16 @@ package Flock;
 
 import mvc.Command;
 import mvc.Model;
+import mvc.View;
+import randomwalk.RandomView;
 import simstation.*;
 
 public class FlockingFactory  extends SimulationFactory{
 
     public Model makeModel() { return new FlockingSimulation(); }
     public String getTitle() { return "Flocking Simulation";}
+    public View makeView(Model model) {return new FlockingView(model);}
+
 
 
 }
